d3e58b92cf8b24add5ff83a83bd3990d0af78188
Nicholas Oxford <nicholasoxford@onepeloton.com>
2020-02-28 19:31:31 -0500

    Initial commit

---
+++ src/sketch.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+let theShader
+// this variable will hold our webcam video
+let cam
+
+function preload () {
+  // load the shader
+  theShader = loadShader('assets/webcam.vert', 'assets/webcam.frag')
+}
+
+function setup () {
+  // shaders require WEBGL mode to work
+  createCanvas(710, 400, WEBGL)
+  noStroke()
+
+  cam = createCapture(VIDEO)
+  cam.size(710, 400)
+
+  cam.hide()
+}
+
+function draw () {
+  // shader() sets the active shader with our shader
+  shader(theShader)
+
+  // passing cam as a texture
+  theShader.setUniform('tex0', cam)
+
+  // rect gives us some geometry on the screen
+  rect(0, 0, width, height)
+}
